a4b534a83aadbb6f58f533eadffb91da147623d0
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2020-05-23 20:01:45 +0000

    refactor wip

--- contracts/Arithmetic.sol
+++ contracts/Arithmetic.sol
@@ -6,3 +6,6 @@ library Arithmetic {
     function mul256By256(uint a, uint b)
         constant
         returns (uint ab32, uint ab1, uint ab0)
+    {
+        uint ahi = a >> 128;
+        uint alo = a & 2**128-1;
