3f4576dee2f606c6970a02340b12d5946bb39588
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2020-09-17 17:52:47 +0000

    src/Builder4/ProjectSettings.bjs src/Builder4/ProjectSettings.vala

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -18,3 +18,6 @@ contract Migrations {
   }
 
   function upgrade(address new_address) restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
+  }
