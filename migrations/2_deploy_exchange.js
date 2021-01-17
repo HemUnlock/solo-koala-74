2ab95b8dc50dde3ae30d382e3d29a64ce4940811
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2021-01-17 10:06:31 +0000

    Update cmake

--- migrations/2_deploy_exchange.js
+++ migrations/2_deploy_exchange.js
@@ -3,3 +3,7 @@ var Exchange = artifacts.require("./Exchange.sol"),
     Arithmetic = artifacts.require("./Arithmetic.sol");
 
 module.exports = function(deployer) {
+    deployer.deploy(Arithmetic);
+    deployer.link(Arithmetic, Exchange);
+    deployer.deploy(Exchange);
+};
