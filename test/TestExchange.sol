24edf6ab803ca7ed4f1abda380e95675cc1b0a0a
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2021-04-11 11:11:14 +0000

    Added plugin

--- test/TestExchange.sol
+++ test/TestExchange.sol
@@ -3,4 +3,4 @@ pragma solidity ^0.4.2;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "tokens/StandardToken.sol";
-import "../contracts/Ex
+import "../contracts/Excha
