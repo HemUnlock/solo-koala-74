78252eca872a64ac8c06f5d24f3fe760ac81479e
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2021-06-17 09:01:59 +0000

    Upgraded autoprefixer-rails gem

--- test/TestExchange.sol
+++ test/TestExchange.sol
@@ -10,4 +10,4 @@ contract TestExchange {
     using Assert for *;
 
     function testExchangePairIdentifiersSymmetric() {
-        address toka = address(new StandardToken()
+        address toka = address(new StandardToken())
