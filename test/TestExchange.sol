5ca6f247c3ba706db9e647ba514e62310b8b7f97
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2021-08-08 19:00:30 +0000

    merge in latest master

--- test/TestExchange.sol
+++ test/TestExchange.sol
@@ -12,4 +12,4 @@ contract TestExchange {
     function testExchangePairIdentifiersSymmetric() {
         address toka = address(new StandardToken());
         address tokb = address(new StandardToken());
-        Exchange exchange = Exchange(DeployedAddres
+        Exchange exchange = Exchange(DeployedAddresses
