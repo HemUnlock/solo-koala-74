63e8d65700ee561d0f17c27a5654f4a5860b0cc2
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2021-10-13 07:58:04 +0000

    adapt

--- test/TestExchange.sol
+++ test/TestExchange.sol
@@ -14,4 +14,4 @@ contract TestExchange {
         address tokb = address(new StandardToken());
         Exchange exchange = Exchange(DeployedAddresses.Exchange());
         exchange.calcExchangeIdentifier([toka, tokb])
-            .equal(exchange.calcExchangeIdentifier([tokb
+            .equal(exchange.calcExchangeIdentifier([tokb, tok
