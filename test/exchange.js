2096b6ade34e1f92b8fc740e099a60dd3b63fbaf
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2023-02-20 22:47:46 +0000

    Remove some unused code.

--- test/exchange.js
+++ test/exchange.js
@@ -3,3 +3,6 @@ var Exchange = artifacts.require("./Exchange.sol");
 
 contract('Exchange', function(accounts) {
   // it("should put 10000 Exchange in the first account", function() {
+  //   return Exchange.deployed().then(function(instance) {
+  //     return instance.getBalance.call(accounts[0]);
+  //   }).then(function(balance) {
