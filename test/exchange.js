cdc810c72d70d4b2d884a5073aaa61f63846d6cd
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2023-02-21 13:07:14 +0000

    drm/radeon/dce6: add missing display reg for tiling setup

--- test/exchange.js
+++ test/exchange.js
@@ -6,3 +6,5 @@ contract('Exchange', function(accounts) {
   //   return Exchange.deployed().then(function(instance) {
   //     return instance.getBalance.call(accounts[0]);
   //   }).then(function(balance) {
+  //     assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+  //   });
