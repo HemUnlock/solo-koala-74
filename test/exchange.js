ab78d8680d9a8779737cc1c193b9437e524efa4a
HemUnlock <107244349+HemUnlock@users.noreply.github.com>
2023-03-02 18:45:35 +0000

    Bumped version to 1.4.0

--- test/exchange.js
+++ test/exchange.js
@@ -20,3 +20,9 @@ contract('Exchange', function(accounts) {
   //   }).then(function(outCoinBalance) {
   //     metaCoinBalance = outCoinBalance.toNumber();
   //     return meta.getBalanceInEth.call(accounts[0]);
+  //   }).then(function(outCoinBalanceEth) {
+  //     metaCoinEthBalance = outCoinBalanceEth.toNumber();
+  //   }).then(function() {
+  //     assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, "Library function returned unexpected function, linkage may be broken");
+  //   });
+  // });
